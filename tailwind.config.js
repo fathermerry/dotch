b9a3282e4c5e349674ccf8a19a0f02df2817e742
Opemipo Aikomo <opemipoaikomo@gmail.com>
2024-04-10 21:47:18 +0100

    Configure colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    colors: {
+      'platinum': '#F1F1F3',
+      'light-gray': "#CACBD0",
+      'slate-gray': '#606272',
+      'dark-indigo': '#2B2D42',
+      'navy-blue': '#191A26',
+      'crimson': '#E53535',
+      'marigold': '#F6C165',
+      'turquoise': '#08C4B2'
+    },
+  }
 }
