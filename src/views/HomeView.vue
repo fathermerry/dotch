8943eb64375ba75dfa30cc6e3c71c0e7be7f12df
Opemipo Aikomo <opemipoaikomo@gmail.com>
2024-04-10 22:31:55 +0100

    Style login

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <a href="/expenses">Login</a>
+  <div class="flex h-screen bg-gray-900 justify-center items-center">
+    <a href="/expenses" class="text-white font-bold text-2xl hover:underline">
+      Login
+    </a>
   </div>
 </template>
 
